49360557d16f3a46bb68d2598fec67aabb845eaa
dshaulov1 <dshaulov@cse-healthcare.io>
2023-11-12 16:49:37 +0100

    FEAT: Change font for better view

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import pyfiglet
 from colorama import Fore, Back, Style
 
 
-result = pyfiglet.figlet_format("SpaceShips",font="shadow") 
+result = pyfiglet.figlet_format("SpaceShips",font="computer") 
 print(Fore.RED + Back.GREEN + "\n" + "\n" + "\n" + result)
 print(Style.RESET_ALL)
 print("\n" + "Welcome to Spaceships, a variant of the classic BattleShip game")
