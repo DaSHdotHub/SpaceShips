4a57d5b9ea4726815b84aa2189ebb5a3c434084b
dshaulov1 <dshaulov@cse-healthcare.io>
2023-11-12 21:03:56 +0100

    REFACTOR: Small refactoring, style is stored into a constant, syntax

--- run.py
+++ run.py
@@ -4,10 +4,12 @@ import pyfiglet
 #import colorama for color styling of the console
 from colorama import Fore, Back, Style
 
+BLUE_WHITE_STYLE = Fore.WHITE + Back.BLUE
+
 
 def create_battlefield(size):
     """
-    Create a battlefield of a given size filled with empty space (-).
+    Create a battlefield of a given size filled with empty space ' - '.
 
     Args:
         size (int): The length and width of the battlefield.
@@ -25,21 +27,22 @@ def print_battlefield(battlefield):
     Args:
         battlefield (list of list): The 2D list representing the battlefield.
     """
-    top_indices = '   | ' + ' | '.join(string.ascii_uppercase[:len(battlefield)]) + ' |'
-    print(Fore.WHITE + Back.BLUE + top_indices + Style.RESET_ALL)
+    top_indices = '   || ' + ' | '.join(string.ascii_uppercase[:len(battlefield)]) + ' |'
+    print(BLUE_WHITE_STYLE + top_indices + Style.RESET_ALL)
     
     for i, row in enumerate(battlefield):
-        print(Fore.WHITE + Back.BLUE + f"{i + 1:2d}", end=' ' + Style.RESET_ALL)
+        print(BLUE_WHITE_STYLE + f"{i + 1:2d}", end=' ' + Style.RESET_ALL + '|')
         print(" ".join(row) + ' |')
 
 
 def main():
     title = pyfiglet.figlet_format("SpaceShips",font="computer") 
-    print(Fore.RED + Back.GREEN + "\n" + "\n" + "\n" + title)
-    print(Style.RESET_ALL)
-    print("\n" + "Welcome to Spaceships, a variant of the classic BattleShip game")
+    print(BLUE_WHITE_STYLE + "\n" + "\n" + "\n" + title + Style.RESET_ALL)
+    print("\n" + "\n" + "Welcome to Spaceships, a variant of the classic BattleShip game")
     size = int(input("Enter the size of the battlefield: "))
     battlefield = create_battlefield(size)
     print_battlefield(battlefield)
+    
+    
 main()
     
