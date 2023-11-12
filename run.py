7977353e9b62286d99b0a1a100a5652969b10b6f
dshaulov1 <dshaulov@cse-healthcare.io>
2023-11-12 19:22:20 +0100

    FEAT: Get user input, create battlefield, print it to console

--- run.py
+++ run.py
@@ -4,14 +4,22 @@ import pyfiglet
 from colorama import Fore, Back, Style
 
 
+def create_battlefield(size):
+    return [['~' for _ in range(size)] for _ in range(size)]
+
+
+def print_battlefield(battlefield):
+    for row in battlefield:
+        print(" ".join(row))
+
+
 def main():
     title = pyfiglet.figlet_format("SpaceShips",font="computer") 
     print(Fore.RED + Back.GREEN + "\n" + "\n" + "\n" + title)
     print(Style.RESET_ALL)
     print("\n" + "Welcome to Spaceships, a variant of the classic BattleShip game")
-    
+    size = int(input("Enter the size of the battlefield: "))
+    battlefield = create_battlefield(size)
+    print_battlefield(battlefield)
 main()
     
-
-
-
