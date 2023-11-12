5cdba3a439a2b79655a9c43f42c7b4fa2819203d
dshaulov1 <dshaulov@cse-healthcare.io>
2023-11-12 16:36:55 +0100

    FEAT: Add color to console

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 # import pyfiglet module 
-import pyfiglet 
+import pyfiglet
+from colorama import Fore, Back, Style
+
 
 result = pyfiglet.figlet_format("SpaceShips",font="shadow") 
-print(result)
+print(Fore.RED + Back.GREEN + "\n" + "\n" + "\n" + result)
+print(Style.RESET_ALL)
 print("\n" + "Welcome to Spaceships, a variant of the classic BattleShip game")
+
+
